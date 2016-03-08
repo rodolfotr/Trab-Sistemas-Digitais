ca0803fc09964192999d559478ad5a423b69e9d0
rodolfotr <rodolfotrevisol015@gmail.com>
2016-03-08 13:31:00 -0300

    Update Trabalho1.v

--- Trabalho1.v
+++ Trabalho1.v
@@ -7,17 +7,17 @@ module Pisca(
 
 endmodule
 
-module Test;
+module Test(output V);
 
   reg clk;
   wire fio;
   
     always #3 clk <= ~clk;
     
-    Pisca P(clk, fio);
+    Pisca A(clk, fio);
 
   initial begin
-    $dumpvars(0, V);
+    $dumpvars(0, A);
     clk <= 0;
     #300;
     $finish;
