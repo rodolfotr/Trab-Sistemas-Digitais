dd3516bf7d80ee811bf32c7e581740d0b4a58ce0
rodolfotr <rodolfotrevisol015@gmail.com>
2016-03-10 22:06:56 -0300

    Update Trabalho2.v

--- Trabalho2.v
+++ Trabalho2.v
@@ -8,7 +8,7 @@ module TOP(input CLOCK_50, input reset, output LED);
    	cont = 0;
    	LED = 0;
    	end
-      if(cont == 5000) begin
+      if(cont == 500) begin
 	 LED = ~LED;
 	 cont = 0;
       end
@@ -35,7 +35,7 @@ module test;
       #1
       reset <= 0;
       CLOCK_50 <= 0;
-      #100000;
+      #5000;
       $finish;   
    end
 endmodule
