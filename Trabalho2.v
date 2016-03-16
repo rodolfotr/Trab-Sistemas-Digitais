c8c52e542042fec08d5bf204991fc0f34836018b
rodolfotr <rodolfotrevisol015@gmail.com>
2016-03-16 20:49:51 -0300

    Update Trabalho2.v

--- Trabalho2.v
+++ Trabalho2.v
@@ -1,22 +1,18 @@
-module Led (input CLOCK_50, input [0:0]KEY, output [0:0]LEDG);
-   
-   reg [25:0] counter;
-   reg state=0;
-   
-   assign LEDG[0] = state;
-   
-   always @(posedge CLOCK_50) begin
-   	if(KEY[0] == 1) begin
-   	counter = 0;
-   	state = 0;
-   	end else begin
-      		if(counter == 50000000) begin
-	 counter = 0;
-	 state = ~state;
-	end else begin
-		counter <= counter + 1;
-      	end
-      	end
-   end
-   
+module Led(input CLOCK_50, output [1:0]LEDR);
+
+    reg [32:0] counter;
+    reg state = 0;
+    
+    assign LEDR[0] = state;
+    assign LEDR[1] = ~state;
+    
+    always @(posedge CLOCK_50) begin
+        if (counter == 50000000) begin
+            state = ~state;
+            counter = 0;
+        end else begin
+            counter <= counter + 1;
+        end
+    end
+
 endmodule
