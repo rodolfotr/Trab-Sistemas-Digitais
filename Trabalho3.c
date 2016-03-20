a2d9a8bfc76eab8f59e25fb870708f780597c1dc
rodolfotr <rodolfotrevisol015@gmail.com>
2016-03-20 18:50:42 -0300

    Update Trabalho3.c

--- Trabalho3.c
+++ Trabalho3.c
@@ -5,15 +5,15 @@ typedef struct ponto{
 	int x, y;
 } ponto;
 
-int area(ponto a, ponto b, ponto c){
+int area(ponto a, ponto b, ponto c){                                                /// Função verificação da area do triangulo.
 	return abs((a.x*(b.y-c.y) + b.x*(c.y-a.y) + c.x*(a.y-b.y)));
 }
 
-int verificacao(ponto a, ponto b, ponto c, ponto d){
+int verificacao(ponto a, ponto b, ponto c, ponto p){                                /// Função verificação do ponto dentro do triangulo.
 	int total = area(a,b,c);
-	int A1 = area(a,c,d);
-	int A2 = area(a,b,d);
-	int A3 = area(c,d,b);
+	int A1 = area(a,c,p);
+	int A2 = area(a,b,p);
+	int A3 = area(c,p,b);
 
 	if(A1+A2+A3 == total){
 		return 1;
@@ -27,16 +27,20 @@ int main(){
 
 	ponto pt1, pt2, pt3, p;
 
+    printf("Ponto 1 do Triangulo - X e Y\n");                                       /// Obtenção das coordenadas dos 3 pontos que compõe o triangulo.
 	scanf("%d %d", &pt1.x, &pt1.y);
+    printf("Ponto 2 do Triangulo - X e Y\n");
 	scanf("%d %d", &pt2.x, &pt2.y);
+    printf("Ponto 3 do Triangulo - X e Y\n");
 	scanf("%d %d", &pt3.x, &pt3.y);
+    printf("Ponto - X e Y\n");                                                      /// Coordenadas do ponto a ser verificado.
 	scanf("%d %d", &p.x, &p.y);
 
 	if(verificacao(pt1, pt2, pt3, p)){
-		printf("Dentro\n");
+		printf("\n\nDentro\n");
 	}
 	else{
-		printf("Fora\n");
+		printf("\n\nFora\n");
 	}
 
 }
